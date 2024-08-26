8d017465f4f132f89ac59d54a1ae2ad4b819438b
Anthony Spriggs <perspectiveva@gmail.com>
2024-08-26 07:43:33 -0400

    PS-4356: Remove IVs and keys

--- decrypt.js
+++ decrypt.js
@@ -22,9 +22,9 @@ function aesDecrypt(sessionKey, encryptedData, iv) {
 const privateKey = fs.readFileSync(path.join(__dirname, 'private-key.pem'), 'utf8');
 
 // Example encoded values (replace with actual values)
-const encodedEncryptedSessionKey = "UoMqnlLCYlqDnpvfTbL6xJj6UFamk9q4MVLDYln3dbRnOX5dOAX30vJr2R8aVihZ70XArU+cCnCkB6TjVVMWfJq1g+TpK4LJ693aKOBDExpe8lvil0shObC1Pqclr0f73zd3is/AAHxUN1f1bpfPT7EbksF/4LaXXjlez91VGPpQHyJe3uJOl+rmhDScr+b0wP4h6Av//ASrSUbdWK8AyJbNFOe6pKmMtdXe6o9bS7aUHb9eKPRpPBQcUI1Ty0SxDgRO0XL+7ewYMdcDLLdbrjzowW/yyimgxsJl6gCEAEJGSA4Kvp3f6ok0YMLSVPtNblDCAjAMHi/OZWWPQ0v1QQ==";
-const encodedEncryptedData = "G73tFROnNJN25H3paoZDYffHutL7NPK3Uh9k11xwmyc=";
-const encodedIv = "lX3Ab2pBqFZvfN0prwaKfg==";
+const encoded_encrypted_session_key = "INSERT_ENCODED_ENCRYPTED_SESSION_KEY"
+const encoded_encrypted_data = "INSERT_ENCODED_ENCRYPTED_DATA"
+const encoded_iv = "INSERT_ENCODED_IV"
 
 const decryptedSessionKey = decryptSessionKey(encodedEncryptedSessionKey, privateKey);
 const decryptedData = aesDecrypt(decryptedSessionKey, encodedEncryptedData, encodedIv);
